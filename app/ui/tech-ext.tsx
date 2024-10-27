5b6bb4cd4d997cacb28d9f9aa1bec857a6cfb071
deewakar <dakshftw01@gmail.com>
2024-10-27 20:04:37 +0530

    fix: type error

--- app/ui/tech-ext.tsx
+++ app/ui/tech-ext.tsx
@@ -1,4 +1,12 @@
-export const TechTag = ({ children, className = "" }) => {
+import { ReactNode } from "react";
+
+export const TechTag = ({
+  children,
+  className = "",
+}: {
+  children: ReactNode;
+  className?: string;
+}) => {
   return (
     <span
       className={`inline-flex items-center px-2 py-0.5 text-sm rounded-md 
