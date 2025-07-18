2b95a1797ab60b706fd2750bdac0d7e740a4f86a
Deewakar Kumar <dakshftw01@gmail.com>
2025-07-18 16:24:53 +0530

    fix: mobile responsive

--- src/app/(routes)/layout.tsx
+++ src/app/(routes)/layout.tsx
@@ -5,7 +5,7 @@ export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div className="flex max-w-full flex-col py-52">
       <div className="flex flex-grow items-center justify-center">
-        <div className="flex w-full max-w-sm flex-col gap-8 md:max-w-lg">
+        <div className="flex w-full max-w-xs flex-col gap-8 md:max-w-lg">
           <div className="mb-12 flex items-center justify-between">
             <BackButton />
           </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="flex max-w-full flex-col py-52">
       <div className="flex flex-grow items-center justify-center">
-        <div className="flex w-full max-w-sm flex-col gap-12 md:max-w-lg">
+        <div className="flex w-full max-w-xs flex-col gap-12 md:max-w-lg">
           <Main />
           <Navbar />
           <Footer />
