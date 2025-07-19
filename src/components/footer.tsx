87daa3df67bcf6f1899d3bf2036f923abfd04467
Deewakar Kumar <dakshftw01@gmail.com>
2025-07-20 01:20:20 +0530

    fix github link

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -8,7 +8,7 @@ export const Footer = () => {
         <a href="https://x.com/deewakar01" target="_blank">
           <Twitter className="text-secondary-foreground size-4 transition-colors duration-300 hover:text-blue-300" />
         </a>
-        <a href="https://x.com/deewakar01" target="_blank">
+        <a href="https://github.com/deewakar-k" target="_blank">
           <Github className="text-secondary-foreground size-4 transition-colors duration-300 hover:text-blue-500" />
         </a>
         <a href="mailto:deewakar.tech@gmail.com" target="_blank">
