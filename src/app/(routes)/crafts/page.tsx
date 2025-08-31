41abeee7e9d3d7db0af9a96c1195f167de345c71
Deewakar Kumar <dakshftw01@gmail.com>
2025-08-31 16:11:23 +0530

    chore

--- src/app/(routes)/crafts/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Showcase } from "@/components/showcase";
-
-export default function Crafts() {
-  return (
-    <div className="relative h-screen w-full">
-      <Showcase />
-    </div>
-  );
-}
