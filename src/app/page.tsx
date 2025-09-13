4f22c1c2eb96bc98cd680677ac85bf9ec8ab45e6
Deewakar Kumar <dakshftw01@gmail.com>
2025-09-13 12:31:50 +0530

    removed scene

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import Crafts from "@/components/crafts";
 import { Main } from "@/components/main";
 import Projects from "@/components/projects";
-import { RetroScene } from "@/components/retro-scene";
 
 export default function Home() {
   return (
@@ -9,7 +8,6 @@ export default function Home() {
       <div className="flex flex-grow items-center justify-center">
         <div className="flex w-full max-w-xs flex-col gap-20 md:max-w-xl">
           <Main />
-          <RetroScene />
           <Projects />
           <Crafts />
           <div className="blur-gradient-bottom pointer-events-none fixed right-0 bottom-0 left-0 h-[180px]"></div>
